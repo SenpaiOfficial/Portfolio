abcb0e6ba050a8bba588a60a580587a876281025
Aydos <senpai.makeyev@gmail.com>
2021-05-26 01:11:41 +0600

    feat: function for modal window

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
- new WOW().init();
+ const modal = document.querySelector('.modal');
+ const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+ const closeBtn = document.querySelector('.modal__close');
+ const switchModal = () => {
+  modal.classList.toggle('modal--visible');
+ };
+ modalBtn.forEach(element => {
+  element.addEventListener('click', switchModal);
+ });
+
+ closeBtn.addEventListener('click', switchModal);
 });
